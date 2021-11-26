5b37cb39c2bc671e3357df1da31b6445c92ca1ed
Geza Denes <denesgeza@gmail.com>
2021-11-26 09:16:01 +0200

    modified challenge

--- summary-challenge.py
+++ summary-challenge.py
@@ -1,12 +1,5 @@
-print('Please choose from the list below: ')
-print('1.\tLearn Python')
-print('2.\tTake a break')
-print('3.\tGet a new job')
-print('4.\tQuit smokking')
-print('0.\tExit')
-
 choice = '-'
-while choice == '0':
+while choice != '0':
     if choice in '12345':
         print('You chose {}'.format(choice))
     else:
@@ -17,6 +10,5 @@ while choice == '0':
         print('4.\tQuit smokking')
         print('0.\tExit')
 
-
-
+    choice = input()
 
